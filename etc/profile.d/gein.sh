24f25aeae4fd2eee1dce7390ec7c06f1b3c3617e
John <jcmdln@gmail.com>
2017-11-30 20:56:21 -0500

    Cleanup

--- etc/profile.d/gein.sh
+++ etc/profile.d/gein.sh
@@ -1,24 +1,5 @@
 #!/bin/env sh
 
-gein-extras() {
-    read -ep "Install gein-extras? [Yes/No]: " Install
-    if $Install | grep -iq "^n"; then exit; fi
-
-    Wget="wget -q"
-    Base_URL="https://raw.githubusercontent.com/jcmdln/gein/master"
-
-    $Wget $Base_URL/etc/emacs/default.el   -O /etc/emacs/default.el
-    $Wget $Base_URL/etc/profile            -O /etc/sudoers
-    $Wget $Base_URL/etc/profile.d/alias.sh -O /etc/profile.d/alias
-    $Wget $Base_URL/etc/sudoers            -O /etc/sudoers
-    $Wget $Base_URL/etc/tmux.conf          -O /etc/tmux.conf
-    $Wget $Base_URL/etc/vimrc              -O /etc/vimrc
-
-    unset Wget
-    unset Base_URL
-}
-
-
 gein-update() {
     emerge --sync
     emerge -av -uDN @system
