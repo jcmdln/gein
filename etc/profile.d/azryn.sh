fb498cfa450d9976b3ebb819f73e0bd083da83eb
John <jcmdln@gmail.com>
2017-12-25 00:21:49 -0500

    Add reconfig

--- etc/profile.d/azryn.sh
+++ etc/profile.d/azryn.sh
@@ -12,6 +12,41 @@ azryn() {
             sudo revdep-rebuild
             ;;
 
+        reconfig)
+            echo "azryn: WARNING: This will overwrite the following scripts:"
+            echo "/etc/portage/repos.conf/gentoo.conf"
+            echo "/etc/profile"
+            echo "/etc/profile.d/alias.sh"
+            echo "/etc/profile.d/azryn.sh"
+            echo "/etc/profile.d/environment.sh"
+            echo "/etc/Xresources"
+            echo "/etc/emacs/default.el"
+            echo "/etc/i3/config"
+            echo "/etc/sudoers"
+            echo "/etc/tmux.conf"
+            echo "/etc/vimrc"
+            echo "/etc/xinitrc"
+            read -ep "Proceed with replacing configurations? [Y/N]: " Proceed
+            if echo $Proceed | grep -iq "^n"; then exit; fi
+            wget -q $BaseUrl/etc/portage/repos.conf/gentoo.conf \
+                 -O /etc/portage/repos.conf/gentoo.conf
+            wget -q $BaseUrl/etc/profile \
+                 -O /etc/profile
+            wget -q $BaseUrl/etc/profile.d/alias.sh \
+                 -O /etc/profile.d/alias.sh
+            wget -q $BaseUrl/etc/profile.d/azryn.sh \
+                 -O /etc/profile.d/azryn.sh
+            wget -q $BaseUrl/etc/profile.d/environment.sh \
+                 -O /etc/profile.d/environment.sh
+            wget -q $BaseUrl/etc/Xresources       -O /etc/Xresources
+            wget -q $BaseUrl/etc/emacs/default.el -O /etc/emacs/default.el
+            wget -q $BaseUrl/etc/i3/config        -O /etc/i3/config
+            wget -q $BaseUrl/etc/sudoers          -O /etc/sudoers
+            wget -q $BaseUrl/etc/tmux.conf        -O /etc/tmux.conf
+            wget -q $BaseUrl/etc/vimrc            -O /etc/vimrc
+            wget -q $BaseUrl/etc/xinitrc          -O /etc/xinitrc
+            ;;
+
         remove|-r)
             sudo emerge -avc --quiet-build ${@:2} && \
             sudo revdep-rebuild
@@ -33,12 +68,13 @@ azryn() {
 
         *)
             echo "Available options:"
-            echo "  cleanup, -c    Remove unneeded packages"
-            echo "  install, -i    Install a package"
-            echo "  remove,  -r    Safely remove a package"
-            echo "  sync,    -s    Sync portage"
-            echo "  update,  -u    Update @world without rebuild"
-            echo "  upgrade, -U    Update @system and @world with rebuild"
+            echo "  cleanup,  -c    Remove unneeded packages"
+            echo "  install,  -i    Install a package"
+            echo "  reconfig, -R    Get latest configuration files"
+            echo "  remove,   -r    Safely remove a package"
+            echo "  sync,     -s    Sync portage"
+            echo "  update,   -u    Update @world without rebuild"
+            echo "  upgrade,  -U    Update @system and @world with rebuild"
             ;;
     esac
 }
