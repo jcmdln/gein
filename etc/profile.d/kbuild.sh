267bdada4afc952e8191fe4407e3d661a11c81f5
jcmdln <jcmdln@gmail.com>
2018-03-20 19:38:30 -0400

    kbuild.sh: Fix typo; return to previous directory

--- etc/profile.d/kbuild.sh
+++ etc/profile.d/kbuild.sh
@@ -1,6 +1,8 @@
 # /etc/profile.d/kbuild.sh
 
 kbuild() {
+    CWD="$(pwd)"
+
     if [ $EUID -ne 0 ]; then
         if [ -e $(command -v sudo) ]; then
             SU="sudo"
@@ -14,19 +16,23 @@ kbuild() {
     eselect kernel list
     TargetProfile=""
     while [ -z "$TargetProfile" ]; do
-	read -ep "Which profile?: " TargetProfile
+        read -ep "Which profile?: " TargetProfile
     done
 
     if [ -n "$TargetProfile" ]; then
-	$SU eselect profile set "$TargetProfile" &&
-	cd /usr/src/linux &&
+        $SU eselect kernel set "$TargetProfile" &&
+        cd /usr/src/linux &&
         $SU make menuconfig &&
-        $SU $KMake         && $SU $KMake modules &&
-        $SU $KMake install && $SU $KMake modules install &&
+        $SU $KMake &&
+        $SU $KMake modules &&
+        $SU $KMake install &&
+        $SU $KMake modules install &&
         $SU grub-mkconfig -o /boot/grub/grub.cfg &&
         $SU emerge -av --quiet-build @module-rebuild
     fi
 
-    make clean
+    $SU make clean
     unset KMake
+
+    cd $CWD
 }
