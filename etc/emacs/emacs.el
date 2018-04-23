ee7c3d5de10a4e59549121ab28d5467004a0e529
John <13183098+jcmdln@users.noreply.github.com>
2018-04-23 18:58:15 -0400

    emacs.el: Improve style; disable more junk

--- etc/emacs/emacs.el
+++ etc/emacs/emacs.el
@@ -8,10 +8,10 @@
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (when (fboundp 'tool-bar-mode)   (tool-bar-mode   -1))
 
-(setq initial-scratch-message nil
-      inhibit-splash-screen t
+(setq initial-scratch-message     nil
+      inhibit-splash-screen       t
       inhibit-startup-buffer-menu t
-      custom-file "~/.emacs.d/custom.el")
+      custom-file                 "~/.emacs.d/custom.el")
 
 ;; Environment Variables
 (setenv "EDITOR"         "emacsclient")
@@ -23,6 +23,7 @@
 (setenv "PROMPT_COMMAND" "")
 (setenv "SHELL"          (getenv "SHELL"))
 (setenv "TERM"           (getenv "TERM"))
+
 
 ;;;
 ;;; Appearance
@@ -40,43 +41,43 @@
 ;;; Normalization
 ;;;
 
-(setq auto-window-vscroll nil
-      c-basic-offset 2
-      column-number-mode t
-      cperl-indent-level 2
-      indent-tabs-mode nil
-      js-indent-level 2
-      prefer-coding-system 'utf-8
-      require-final-newline t
+(setq auto-window-vscroll                 nil
+      c-basic-offset                      2
+      column-number-mode                  t
+      cperl-indent-level                  2
+      indent-tabs-mode                    nil
+      js-indent-level                     2
+      prefer-coding-system                'utf-8
+      require-final-newline               t
       save-interprogram-paste-before-kill t
-      scroll-conservatively 101
-      scroll-down-aggressively 0.0
-      scroll-margin 0
-      scroll-preserve-screen-position 1
-      scroll-step 1
-      scroll-up-aggressively 0.0
-      select-enable-primary nil
-      set-default-coding-systems 'utf-8
-      set-language-environment "UTF-8"
-      set-locale-environment "en_US.UTF-8"
-      show-paren-delay 0
-      tab-width 2
-      visible-bell t)
+      scroll-conservatively               101
+      scroll-down-aggressively            0.0
+      scroll-margin                       0
+      scroll-preserve-screen-position     1
+      scroll-step                         1
+      scroll-up-aggressively              0.0
+      select-enable-primary               nil
+      set-default-coding-systems          'utf-8
+      set-language-environment            "UTF-8"
+      set-locale-environment              "en_US.UTF-8"
+      show-paren-delay                    0
+      tab-width                           2
+      visible-bell                        t)
 
 (global-visual-line-mode t)
-(show-paren-mode t)
-(fset 'yes-or-no-p 'y-or-n-p)
+(show-paren-mode         t)
+(fset 'yes-or-no-p       'y-or-n-p)
 
 
 ;;;
 ;;; Input
 ;;;
 
-(setq mouse-wheel-follow-mouse 't
+(setq mouse-wheel-follow-mouse      't
       mouse-wheel-progressive-speed nil
-      mouse-wheel-scroll-amount '(3 ((shift) . 3))
-      mouse-yank-at-point t
-      xterm-mouse-mode t)
+      mouse-wheel-scroll-amount     '(3 ((shift) . 3))
+      mouse-yank-at-point           t
+      xterm-mouse-mode              t)
 
 ;; Buffers
 (global-set-key (kbd "C-x x")           'kill-buffer-and-window)
@@ -153,14 +154,14 @@
   :config
   (add-hook 'prog-mode-hook 'company-mode)
   (add-hook 'text-mode-hook 'company-mode)
-  (setq company-tooltip-limit 20
-        company-idle-delay .3
-        company-echo-delay 0
+  (setq company-tooltip-limit  20
+        company-idle-delay     0.3
+        company-echo-delay     0
         company-begin-commands '(self-insert-command)))
 
 ;; (use-package company-ansible)
 ;; (use-package company-emoji)
-;; (use-package company-go)
+(use-package company-go)
 
 (use-package company-irony
   :config (add-to-list 'company-backends 'company-irony))
@@ -204,14 +205,12 @@
 (use-package flycheck
   :config (add-hook 'prog-mode-hook 'flycheck-mode))
 
-(use-package flymake)
-
 (use-package flyspell
   :config
   (add-hook 'flyspell-mode-hook (auto-dictionary-mode 1))
   (add-hook 'markdown-mode-hook 'flyspell-mode)
-  (add-hook 'prog-mode-hook 'flyspell-prog-mode)
-  (add-hook 'text-mode-hook 'flyspell-mode))
+  (add-hook 'prog-mode-hook     'flyspell-prog-mode)
+  (add-hook 'text-mode-hook     'flyspell-mode))
 
 (use-package highlight-indent-guides
   :config
@@ -223,14 +222,14 @@
   :config
   (add-hook 'ibuffer-hook
             (lambda()
-              (ibuffer-auto-mode t)
-              (ibuffer-do-sort-by-alphabetic)
-              (ibuffer-do-sort-by-major-mode))))
+              (ibuffer-auto-mode             t)
+              (ibuffer-do-sort-by-alphabetic t)
+              (ibuffer-do-sort-by-major-mode t))))
 
 (use-package irony
   :config
-  (add-hook 'c-mode-hook     'irony-mode)
   (add-hook 'c++-mode-hook   'irony-mode)
+  (add-hook 'c-mode-hook     'irony-mode)
   (add-hook 'objc-mode-hook  'irony-mode)
   (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
   (define-key irony-mode-map [remap completion-at-point] 'counsel-irony)
@@ -262,14 +261,14 @@
   (add-hook 'prog-mode-hook     'rainbow-delimiters-mode)
   (add-hook 'text-mode-hook     'rainbow-delimiters-mode))
 
-(use-package smartparens
-  :config
-  (add-hook 'markdown-mode-hook 'smartparens-mode)
-  (add-hook 'prog-mode-hook 'smartparens-mode)
-  (add-hook 'text-mode-hook 'smartparens-mode)
-  (setq sp-highlight-pair-overlay nil
-        sp-highlight-wrap-overlay nil
-        sp-highlight-wrap-tag-overlay nil))
+;; (use-package smartparens
+;;   :config
+;;   (add-hook 'markdown-mode-hook 'smartparens-mode)
+;;   (add-hook 'prog-mode-hook     'smartparens-mode)
+;;   (add-hook 'text-mode-hook     'smartparens-mode)
+;;   (setq sp-highlight-pair-overlay     nil
+;;         sp-highlight-wrap-overlay     nil
+;;         sp-highlight-wrap-tag-overlay nil))
 
 (use-package server
   :bind ("C-x C-c" . server-stop)
@@ -320,14 +319,14 @@
 
 ;;   (setq circe-default-part-message ""
 ;;         circe-default-quit-message ""
-;;         circe-format-server-topic "*** Topic: {userhost}: {topic-diff}"
-;;         circe-reduce-lurker-spam t
+;;         circe-format-server-topic  "*** Topic: {userhost}: {topic-diff}"
+;;         circe-reduce-lurker-spam   t
 ;;         circe-use-cycle-completion t
-;;         lui-fill-type nil
-;;         lui-flyspell-alist '((".*" "american"))
-;;         lui-flyspell-p t
-;;         lui-time-stamp-format "%H:%M:%S"
-;;         lui-time-stamp-position 'left-margin)
+;;         lui-fill-type              nil
+;;         lui-flyspell-alist         '((".*" "american"))
+;;         lui-flyspell-p             t
+;;         lui-time-stamp-format      "%H:%M:%S"
+;;         lui-time-stamp-position    'left-margin)
 
 ;;   (defun my-lui-setup()
 ;;     (setq fringes-outside-margins t
@@ -338,7 +337,7 @@
 ;;   (defun my-circe-prompt()
 ;;     (lui-set-prompt
 ;;      (concat (propertize
-;;               (concat (buffer-name) ">")
+;;               (concat (buffer-name) ":")
 ;;               'face 'circe-prompt-face) " ")))
 
 ;;   (defun my-circe-message-option-chanserv (nick user host command args)
@@ -346,7 +345,7 @@
 ;;                (string-match "^\\[#.+?\\]" (cadr args)))
 ;;       '((dont-display . t))))
 
-;;   (add-hook 'circe-chat-mode-hook 'my-circe-prompt)
+;;   (add-hook 'circe-chat-mode-hook           'my-circe-prompt)
 ;;   (add-hook 'circe-message-option-functions 'my-circe-message-option-chanserv)
 ;;   (add-hook 'lui-mode-hook (lambda() (my-lui-setup) (my-circe-set-margin))))
 
@@ -360,28 +359,27 @@
 
 (use-package eshell
   :config
-  (setq eshell-cmpl-cycle-completions nil
-        eshell-error-if-no-glob t
-        eshell-hist-ignoredups t
-        eshell-history-size 4096
-        eshell-prefer-lisp-functions t
-        eshell-save-history-on-exit t
-        eshell-scroll-to-bottom-on-input nil
+  (setq eshell-cmpl-cycle-completions     nil
+        eshell-error-if-no-glob           t
+        eshell-hist-ignoredups            t
+        eshell-history-size               4096
+        eshell-prefer-lisp-functions      t
+        eshell-save-history-on-exit       t
+        eshell-scroll-to-bottom-on-input  nil
         eshell-scroll-to-bottom-on-output nil
         eshell-scroll-show-maximum-output nil
-
-        eshell-visual-commands
-        '("alsamixer" "atop" "htop" "less" "mosh" "nano" "ssh" "tail"
-          "top" "vi" "vim" "watch" )
-
-        eshell-prompt-regexp "^[^#$\n]*[#$] "
+        eshell-prompt-regexp              "^[^#$\n]*[#$] "
         eshell-prompt-function
         (lambda nil
           (concat
            "[" (user-login-name) "@" (system-name) " "
            (if (string= (eshell/pwd) (getenv "HOME"))
                "~" (eshell/basename (eshell/pwd))) "]"
-           (if (= (user-uid) 0) "# " "$ "))))
+           (if (= (user-uid) 0) "# " "$ ")))
+
+        eshell-visual-commands
+        '("alsamixer" "atop" "htop" "less" "mosh" "nano" "ssh" "tail"
+          "top" "vi" "vim" "watch" ))
 
   (defun eshell/clear()
     (interactive)
@@ -395,7 +393,7 @@
 (use-package eww
   :config
   (setq browse-url-browser-function 'eww-browse-url
-        shr-blocked-images "")
+        shr-blocked-images          "")
 
   (defun eww-toggle-images()
     "Toggle blocking images in eww."
@@ -502,13 +500,13 @@
 ;;   (add-hook 'before-save-hook 'gofmt-before-save)
 ;;   (add-hook 'go-mode-hook
 ;;             (lambda()
-;;               (setq tab-width 4
+;;               (setq tab-width        4
 ;;                     indent-tabs-mode 1)
 ;;               (set (make-local-variable 'company-backends) '(company-go))
 ;;               (company-mode t))))
 
 ;; (use-package gradle-mode)
-(use-package json-mode)
+;; (use-package json-mode)
 ;; (use-package jsx-mode)
 ;; (use-package less-css-mode)
 ;; (use-package lua-mode)
